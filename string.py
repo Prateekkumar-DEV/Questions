816c2d47461c2040f1fd0b727b997941c0cbb248
Prateekkumar-DEV <162008504+Prateekkumar-DEV@users.noreply.github.com>
2024-03-03 20:38:18 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,19 +1,43 @@
-def count_char(s):
-    c = {}
-    for i in "COOKOFF":
-        c[i] = s.count(char)
-    return c
+alpha = ['C','O','K','F']
 
-def ans(s1, s2):
-    count_s1 = count_char(s1)
-    count_s2 = count_char(s2)
+def aplha_true(s):
+    case_1 = False
+    for i in s:
+        if i not in alpha:
+            case_1 = False
+        else:
+            case_1 = True
+    return case_1
     
-    for j in count_s1:
-        if count_s2[j] < count_s1[j]:
-            return "NO"
-    return "YES"
+def count_char(s):
+    count = 0
+    for i in s:
+        count = count + 1
+    return count
 
-s1 = input()
-s2 = input()
+def count_check(x,y):
+    case = False
+    if (count_char(x) % count_char(y)) == 0:
+        n = count_char(x) / count_char(y)
+        for i in alpha:
+            if x.count(i) == n:
+                case = True
+    return case
+    
+def check(a, b):
+    if (aplha_true(a) & aplha_true(b)) == False:
+        case_2 = False
+    
+    case_2 = count_check(s1,s2)
+    
+    return case_2
 
-print(ans(s1, s2))
+s1 = input().upper()
+s2 = input().upper()
+
+if check(s1,s2) == True:
+    ans = "Yes"
+else:
+    ans = "No"
+
+print(ans)
